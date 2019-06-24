6a6f7245f01810b83053ae3700e43427353b8699
Richard Silver <richard@pyx.is>
2019-06-23 17:27:26 -0700

    first commit

---
+++ backend/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"go-react-websocket/websocket"
+	
+)
+
+func serveWs(w http.ResponseWriter, r *http.Request) {
+	ws, err := websocket.Upgrade(w, r)
+	if err != nil {
+		fmt.Fprintf(w, "%+V\n", err)
+	}
+	go websocket.Writer(ws)
+	websocket.Reader(ws)
+}
+
+func setupRoutes() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Simple Server")
+	})
+	http.HandleFunc("/ws", serveWs)
+}
+
+func main() {
+	setupRoutes()
+	http.ListenAndServe(":8080", nil)
+}
